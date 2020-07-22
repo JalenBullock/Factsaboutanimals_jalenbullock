906f8ef32972c63ebb1f1d89c0053a56e89ba83d
JalenBullock <JalenB846@gmail.com>
2020-07-22 15:30:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+Fact1: (Cats are Prickly)
+Fact2: ("Cats can right themselves when they're falling")
+Fact3: ("Cats sleep about 16 hours a day")
+Fact4: ("Cats can have whiskers on their chins, cheeks, eyebrows and front legs")
+
+print(Fact1: ("Cats are Prickly")
+print(Fact2: "Cats can right themselves when they're falling")
+print(Fact3: "Cats sleep about 16 hours a day")
+print(Fact4: ("Cats can have whiskers on their chins, cheeks, eyebrows and front legs")
